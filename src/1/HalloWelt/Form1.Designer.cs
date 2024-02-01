cf7adbd64497ade3a2c221b78905c549e12a4199
Sascha Blimke <sascha.blimke@web.de>
2024-02-01 11:15:57 +0100

    Hallo Welt abgeschlossen

--- src/1/HalloWelt/Form1.Designer.cs
+++ src/1/HalloWelt/Form1.Designer.cs
@@ -28,11 +28,44 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        BtnHalloWelt = new Button();
+        LblAnzeige = new Label();
+        SuspendLayout();
+        // 
+        // BtnHalloWelt
+        // 
+        BtnHalloWelt.Location = new Point(35, 59);
+        BtnHalloWelt.Name = "BtnHalloWelt";
+        BtnHalloWelt.Size = new Size(108, 23);
+        BtnHalloWelt.TabIndex = 0;
+        BtnHalloWelt.Text = "Hallo, Welt!";
+        BtnHalloWelt.UseVisualStyleBackColor = true;
+        BtnHalloWelt.Click += BtnHalloWelt_Click;
+        // 
+        // LblAnzeige
+        // 
+        LblAnzeige.AutoSize = true;
+        LblAnzeige.Location = new Point(175, 67);
+        LblAnzeige.Name = "LblAnzeige";
+        LblAnzeige.Size = new Size(38, 15);
+        LblAnzeige.TabIndex = 2;
+        LblAnzeige.Text = "label2";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(LblAnzeige);
+        Controls.Add(BtnHalloWelt);
+        Name = "Form1";
+        Text = "HalloWelt";
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private Button BtnHalloWelt;
+    private Label LblAnzeige;
 }
