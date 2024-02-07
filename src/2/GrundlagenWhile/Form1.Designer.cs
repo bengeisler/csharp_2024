4bba705dc49725e7534ac9a2aede0dd306e50ac5
Benedikt Geisler <benedikt.geisler@posteo.de>
2024-02-07 08:19:42 +0100

    Grundlagen While

---
+++ src/2/GrundlagenWhile/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace GrundlagenWhile
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LblAnzeige = new Label();
+            CmdAnzeigen1 = new Button();
+            CmdAnzeigen2 = new Button();
+            SuspendLayout();
+            // 
+            // LblAnzeige
+            // 
+            LblAnzeige.AutoSize = true;
+            LblAnzeige.Location = new Point(12, 9);
+            LblAnzeige.Name = "LblAnzeige";
+            LblAnzeige.Size = new Size(59, 25);
+            LblAnzeige.TabIndex = 0;
+            LblAnzeige.Text = "label1";
+            // 
+            // CmdAnzeigen1
+            // 
+            CmdAnzeigen1.Location = new Point(332, 7);
+            CmdAnzeigen1.Name = "CmdAnzeigen1";
+            CmdAnzeigen1.Size = new Size(112, 34);
+            CmdAnzeigen1.TabIndex = 1;
+            CmdAnzeigen1.Text = "Anzeigen 1";
+            CmdAnzeigen1.UseVisualStyleBackColor = true;
+            CmdAnzeigen1.Click += CmdAnzeigen1_Click;
+            // 
+            // CmdAnzeigen2
+            // 
+            CmdAnzeigen2.Location = new Point(332, 47);
+            CmdAnzeigen2.Name = "CmdAnzeigen2";
+            CmdAnzeigen2.Size = new Size(112, 34);
+            CmdAnzeigen2.TabIndex = 2;
+            CmdAnzeigen2.Text = "Anzeigen 2";
+            CmdAnzeigen2.UseVisualStyleBackColor = true;
+            CmdAnzeigen2.Click += CmdAnzeigen2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(473, 359);
+            Controls.Add(CmdAnzeigen2);
+            Controls.Add(CmdAnzeigen1);
+            Controls.Add(LblAnzeige);
+            Name = "Form1";
+            Text = "Schleifen mit while und do..while";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LblAnzeige;
+        private Button CmdAnzeigen1;
+        private Button CmdAnzeigen2;
+    }
+}
