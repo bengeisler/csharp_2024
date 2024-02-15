3d6b06b40a2e0af115be7dc09257f39ec7999e7a
DanieIK <kroneda1@aol.de>
2024-02-15 09:06:00 +0100

    Klassen

---
+++ src/5/Klassen/Fahrzeug.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen
+{
+    //Klasse erzeugen
+    //ohne private/internal/public: klasse ist automatisch private
+    class Fahrzeug
+    {
+        //Eigenschaft
+        private int _geschwindigkeit = 0;
+
+        //Methode
+        public void Beschleinigen(int geschwindigkeit) => _geschwindigkeit += geschwindigkeit;
+
+        public string Ausgabe() => $"Geschwindigkeit:{_geschwindigkeit}";
+    }
+}
---
+++ src/5/Klassen/Form1.cs
@@ -0,0 +1,36 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Klassen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdButton1_Click(object sender, EventArgs e)
+        {
+            // Neues Fahrzeug erzeugen: Instanz der anlegen
+            //1 Möglichkeit
+            Fahrzeug Vespa = new Fahrzeug();
+            //2 Möglichkeit
+            //Fahrzeug Vespa2=new();
+            ;
+            LblAnzegie1.Text = "Vespa\n" + Vespa.Ausgabe() + "\n";
+            Vespa.Beschleinigen(10);
+            LblAnzegie1.Text += Vespa.Ausgabe() + "\n";
+
+            LblAnzegie1.Text += "\n\n";
+
+            //Weiteres Objekt erzeugen
+            // 2 Möglichkeit
+            Fahrzeug Schwalbe = new();
+            
+            LblAnzegie1.Text += "Schwalbe\n" + Schwalbe.Ausgabe() + "\n";
+            Schwalbe.Beschleinigen(50);
+            LblAnzegie1.Text += Schwalbe.Ausgabe() + "\n";
+        }
+    }
+}
