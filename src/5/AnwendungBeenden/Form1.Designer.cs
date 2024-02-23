a6ee18d213d9af3e40dcc3bc21e8972982f6332e
Benedikt Geisler <benedikt.geisler@posteo.de>
2024-02-23 09:42:37 +0100

    Anwendung beenden

---
+++ src/5/AnwendungBeenden/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace AnwendungBeenden
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            CmdBeenden = new Button();
+            SuspendLayout();
+            // 
+            // CmdBeenden
+            // 
+            CmdBeenden.Location = new Point(121, 12);
+            CmdBeenden.Name = "CmdBeenden";
+            CmdBeenden.Size = new Size(112, 43);
+            CmdBeenden.TabIndex = 0;
+            CmdBeenden.Text = "Beenden";
+            CmdBeenden.UseVisualStyleBackColor = true;
+            CmdBeenden.Click += CmdBeenden_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(371, 76);
+            Controls.Add(CmdBeenden);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button CmdBeenden;
+    }
+}
---
+++ src/5/AnwendungBeenden/Form1.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace AnwendungBeenden
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdBeenden_Click(object sender, EventArgs e)
+        {
+            // Anwendung beenden
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Abfragen, ob wirklich beendet werden soll
+            var ergebnis = MessageBox.Show(
+                "Wollen Sie wirklich beenden?",
+                "Beenden?",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question);
+
+            // Falls Benutzer auf "Abbrechen" geklickt hat wird das
+            //  Schlieﬂen des Formulars abgebrochen
+            if (ergebnis == DialogResult.Cancel)
+                e.Cancel = true;    
+        }
+    }
+}
